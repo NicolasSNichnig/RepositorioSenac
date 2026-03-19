976c411289e978b36e59bd603c5441f293e276ad
NicolasSNichnig <nicolas.s.nichnig@gmail.com>
2026-03-19 20:13:10 -0300

    Atividade do dia 19/03

---
+++ 1° Semestre/Dia 1903/01.py
@@ -0,0 +1,8 @@
+listaCompras = ["Arroz", "Macarrão", "Carne"]
+
+print(listaCompras)
+novoItem = input("Insira o seu novo item: ")
+
+listaCompras.append(novoItem)
+listaCompras.remove("Macarrão")
+print(listaComprasS)
